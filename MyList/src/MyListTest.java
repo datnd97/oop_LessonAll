64e4bca7f116e3d1937dc3495a71ad3524f84fa7
pham dat <mrphamdat099@gmail.com>
2019-09-09 15:36:40 +0700

    [Thực hành] Triển khai lớp List đơn giản

---
+++ MyList/src/MyListTest.java
@@ -0,0 +1,25 @@
+public class MyListTest {
+    public static void main(String[] args) throws IndexOutOfBoundsException{
+        MyList<Integer> listInteger = new MyList<Integer>();
+        try {
+
+            listInteger.add(1);
+            listInteger.add(2);
+            listInteger.add(3);
+//            listInteger.add(4);
+//            listInteger.get(6);
+
+            System.out.println("Element 1: "+listInteger.get(1));
+            System.out.println("Element 2: "+listInteger.get(2));
+//            System.out.println("Element 6: "+listInteger.get(3));
+//            System.out.println("Element 4: "+listInteger.get(4));
+//            System.out.println("Element 6: "+listInteger.get(6));
+            listInteger.get(-1);
+            System.out.println("element -1: "+ listInteger.get(-1));
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("index vuot so size cua Mang");
+        }
+
+    }
+}
